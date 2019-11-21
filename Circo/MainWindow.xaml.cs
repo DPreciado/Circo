0d8cb322d11729d965b51d1401066d02b99de8eb
david arturo preciado osuna <arturo_preciado@live.com.mx>
2019-11-21 10:04:09 -0700

    menú en user control y al darle start si cambia el estado de menu a gameplay

--- Circo/MainWindow.xaml.cs
+++ Circo/MainWindow.xaml.cs
@@ -24,89 +24,33 @@ namespace Circo
     /// </summary>
     public partial class MainWindow : Window
     {
-        WaveIn waveIn; //Conexion con microfono
-        WaveFormat formato; //Fortmato de audio
+        public enum EstadodeJuego { menu, gameplay, gameOver}
+
+        public EstadodeJuego estadoActual = EstadodeJuego.menu;
+
+        
+        
+        public void iniciar()
+        {
+            estadoActual = EstadodeJuego.gameplay;
+        }
+        
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void btnInicio_Click(object sender, RoutedEventArgs e)
-        {
-            //Inicializar la conexion
-            waveIn = new WaveIn();
-
-            //Establecer el formato
-            waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
-            formato = waveIn.WaveFormat;
-
-            //Duracion del buffer
-            waveIn.BufferMilliseconds = 500;
-
-            //Con que funcion respondemos
-            //Cuando se llena el buffer
-            waveIn.DataAvailable += WaveIn_DataAvailable;
-
-            waveIn.StartRecording();
-        }
-
-        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
-        {
-            byte[] buffer = e.Buffer;
-            int bytesGrabados = e.BytesRecorded;
-
-            int numMuestras = bytesGrabados / 2;
-
-            int exponente = 0;
-            int numeroBits = 0;
-
-            do
+            if(estadoActual == EstadodeJuego.menu)
             {
-                exponente++;
-                numeroBits = (int)Math.Pow(2, exponente);
-            } while (numeroBits < numMuestras);
-            exponente -= 1;
-            numeroBits = (int)Math.Pow(2, exponente);
-            Complex[] muestrasComplejas = new Complex[numeroBits];
-
-            for (int i = 0; i < bytesGrabados; i += 2)
-            {
-                short muestra =
-                    (short)(buffer[i + 1] << 8 | buffer[i]);
-                float muestra32bits =
-                    (float)muestra / 32768.0f;
-                if (i / 2 < numeroBits)
-                {
-                    muestrasComplejas[i / 2].X = muestra32bits;
-                }
+                pantallaPrincipal.Children.Add(new Menu(iniciar));
+                
+            
             }
 
-            FastFourierTransform.FFT(true, exponente, muestrasComplejas);
-
-            float[] valoresAbsolutos = new float[muestrasComplejas.Length];
-
-            for (int i = 0; i < muestrasComplejas.Length; i++)
-            {
-                valoresAbsolutos[i] = (float)
-                    Math.Sqrt(
-                    (muestrasComplejas[i].X * muestrasComplejas[i].X) +
-                    (muestrasComplejas[i].Y * muestrasComplejas[i].Y));
-            }
-
-            int indiceValorMaximo =
-                valoresAbsolutos.ToList().IndexOf(
-                valoresAbsolutos.Max());
-
-            float frecuenciaFundamental =
-                (float)(indiceValorMaximo * formato.SampleRate) /
-                (float)valoresAbsolutos.Length;
-
-            lblHertz.Text = frecuenciaFundamental.ToString("N") + " Hz";
+            
         }
 
-        private void btnDetener_Click(object sender, RoutedEventArgs e)
-        {
-            waveIn.StopRecording();
-        }
+        
+
+        
     }
 }
